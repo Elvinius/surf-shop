d3a2a979f464596d4fc19f7953d98d922bc57463
Elvinius <elwinabbasli@gmail.com>
2021-01-30 13:15:33 +0200

    Add models and initial passport configuration

--- routes/index.js
+++ routes/index.js
@@ -47,12 +47,12 @@ router.put('/forgot-pw', (req, res, next) => {
 });
 
 
-/* GET /reset */
+/* GET /reset/:token */
 router.get('/reset/:token', (req, res, next) => {
   res.send("GET /reset/:token");
 });
 
-/* PUT /reset */
+/* PUT /reset/:token */
 router.put('/reset/:token', (req, res, next) => {
   res.send("PUT /reset/:token");
 });
